c5f2cfe4793961f33bc83923fb86e5e8661ea236
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-01 14:55:43 +0900

    [Bronze IV] Title: 숫자의 합, Time: 212 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -5,17 +7,54 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        int n = sc.nextInt();
-        sc.nextLine();
-        String number = sc.nextLine();
-        System.out.println(main.getNumberSum(number, n));
+        Input in = new Input(sc.nextInt(), sc.next());
+
+        List<Integer> numbers = main.toNumberOfDigits(in.getNumber());
+        int result = main.sum(numbers);
+        
+        System.out.println(result);
     }
 
-    private int getNumberSum(String number, int length){
-        int sum = 0;
-        for(int i=0; i<length; i++){
-            sum += Integer.parseInt(String.valueOf(number.charAt(i)));
+    // 문자열로 이루어진 숫자를 각 자리수(정수)로 변환하여 리스트로 반환하는 메소드
+    private List<Integer> toNumberOfDigits(String number){
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<number.length(); i++) {
+            result.add(Integer.parseInt(String.valueOf(number.charAt(i))));
         }
-        return sum;
+        return result;
+    }
+
+    // 리스트를 스트림으로 변환하여 누적 합을 반환하는 메소드
+    private int sum(List<Integer> numbers){
+        return numbers.stream().mapToInt(Integer::intValue).sum();
+    }
+}
+
+/**
+ * 초기 값을 담는 엔티티 클래스
+ */
+class Input {
+    private int count;
+    private String number;
+
+    public Input(int count, String number) {
+        this.count = count;
+        this.number = number;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
