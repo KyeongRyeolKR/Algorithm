941af29ce901491225be559f8e8436940ee3c104
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-23 21:36:51 +0900

    Update 삼각형의 완성조건 （1）.java

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -3,10 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] sides) {
         Arrays.sort(sides);
-        if(sides[2] < sides[0] + sides[1]) {
-            return 1;
-        } else {
-            return 2;
-        }
+        return (sides[2] < sides[0] + sides[1]) ? 1 : 2;
     }
 }
