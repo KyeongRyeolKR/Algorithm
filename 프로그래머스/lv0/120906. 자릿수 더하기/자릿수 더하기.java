828bce3be2cbd37ae333620a8dfef5fd96b5d960
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-27 13:51:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.13 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,19 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n) {
-        String n_ = String.valueOf(n);
-        
-        String[] each_ = n_.split("");
-        
-        int[] each = new int[each_.length];
-        for(int i=0; i<each_.length; i++) {
-            each[i] = Integer.parseInt(each_[i]);
-        }
-        
-        int sum = 0;
-        for(int x : each) {
-            sum += x;
-        }
-        
-        return sum;
+        return Arrays.stream(String.valueOf(n).split(""))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
