07e2986a30d0575c445c3c61282e10e9d7aee4af
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-27 22:26:34 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int hp) {
         int a = hp / 5;
-        int b = (hp - (a * 5)) / 3;
-        int c = (hp - (a * 5) - (b * 3));
+        int b = hp % 5 / 3;
+        int c = hp % 5 % 3;
 
         return a + b + c;
     }
