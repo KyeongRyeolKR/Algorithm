586baab91fbf09210ff4e28853f0bdcef9e79153
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-27 15:20:35 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n) {
-        for (int i = 1; i < n / 2; i++) {
-            if ((i * i) == n) {
-                return 1;
-            }
-        }
-        return 2;
+        return (n % Math.sqrt(n)) == 0 ? 1 : 2;
     }
 }
