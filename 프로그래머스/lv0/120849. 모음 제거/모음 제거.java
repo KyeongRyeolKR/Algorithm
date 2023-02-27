a78df9e15e8f8e90eee10a6615fdbb0a95260fbf
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-02-27 14:38:50 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String[] removes = {"a", "e", "i", "o", "u"};
-
-        for (String remove : removes) {
-            if (my_string.contains(remove)) {
-                my_string = my_string.replaceAll(remove, "");
-            }
-        }
-        
-        return my_string;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
