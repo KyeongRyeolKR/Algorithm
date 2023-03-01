96ca6b3e22703feea63696eb8e8d68cb82cdbcb2
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-01 23:41:49 +0900

    [level 0] Title: 가위 바위 보, Time: 4.28 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,13 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String rsp) {
-        String[] rspArr = rsp.split("");
-        
-        StringBuilder builder = new StringBuilder();
-        for(String s : rspArr) {
-            if(s.equals("2")) builder.append("0");
-            if(s.equals("0")) builder.append("5");
-            if(s.equals("5")) builder.append("2");
-        }
-        return builder.toString();
+        return Arrays.stream(rsp.split(""))
+                .map(e -> e.equals("2") ? "0" : e.equals("0") ? "5" : "2")
+                .collect(Collectors.joining());
     }
 }
