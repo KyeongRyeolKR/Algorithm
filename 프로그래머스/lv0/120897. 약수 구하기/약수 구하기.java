6fbf85807ecf1aadafc19c4c6d9321ab541b1161
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-08 18:00:09 +0900

    [level 0] Title: 약수 구하기, Time: 3.14 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,14 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=1; i<=n; i++) {
-            if(n % i == 0) list.add(i);
-        }
-        return list.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).toArray();
     }
 }
