168c7ef4def3e2d039cb87e3964f69bfbabf3fb4
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-13 21:20:54 +0900

    [level 0] Title: k의 개수, Time: 77.02 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int i, int j, int k) {
-        StringBuilder sb = new StringBuilder();
-        for(int a=i; a<=j; a++){
-            sb.append(a);
-        }
+        String str = IntStream.rangeClosed(i,j)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
 
-        int cnt = 0;
-        for(String x : sb.toString().split("")) {
-            if(x.equals(String.valueOf(k))) cnt++;
-        }
-
-        return cnt;
+        return (int) Arrays.stream(str.split(""))
+                .filter(e -> String.valueOf(k).equals(String.valueOf(e)))
+                .count();
     }
 }
