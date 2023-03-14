902758491e51eeca0c20b95915190522924a5f21
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-14 18:50:07 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 3.26 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,5 +1,9 @@
 import java.util.stream.Collectors;
 
+/**
+ * "Zbcdefg" => "gfedcbZ"
+ * 우선 오름차순으로 작은거부터 정렬시키고, 반대로 뒤집어주면 완성
+ */
 class Solution {
     public String solution(String s) {
         return new StringBuilder(
@@ -7,8 +11,10 @@ class Solution {
                 .sorted()
                 .mapToObj(Character::toString)
                 .collect(Collectors.joining())
-        )
-                .reverse()
-                .toString();
+        ).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("ZabsdSGw"));
     }
 }
