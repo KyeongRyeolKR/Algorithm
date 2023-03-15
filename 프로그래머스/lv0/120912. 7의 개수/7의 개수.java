9b388cfe4ff8899a486c359fdce5d08706005460
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-15 17:09:24 +0900

    [level 0] Title: 7의 개수, Time: 2.51 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -3,11 +3,22 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] array) {
-        return (int) Arrays.stream(Arrays.stream(array)
-                        .mapToObj(String::valueOf)
-                        .collect(Collectors.joining())
-                .split(""))
-                .filter(e -> e.equals("7"))
+//        return (int) Arrays.stream(Arrays.stream(array)
+//                        .mapToObj(String::valueOf)
+//                        .collect(Collectors.joining())
+//                        .split(""))
+//                .filter(e -> e.equals("7"))
+//                .count();
+
+        return (int) Arrays.stream(array)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining())
+                .chars()
+                .filter(e -> e == '7')
                 .count();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{7,77,17}));
     }
 }
