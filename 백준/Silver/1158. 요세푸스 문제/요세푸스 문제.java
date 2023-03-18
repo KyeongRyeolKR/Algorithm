520f3f01d5e571ca8dd82af2d864ed9d32cd2d00
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-18 16:17:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 612 ms, Memory: 297832 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int k = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=1; i<=size; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(!queue.isEmpty()) {
+            for(int i=0; i<k; i++) {
+                if(i != k-1) {
+                    queue.offer(queue.poll());
+                } else {
+                    sb.append(queue.poll());
+                }
+            }
+            if(!queue.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+
+        System.out.println(sb);
+    }
+}
