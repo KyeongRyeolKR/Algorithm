f154db07f941c608a8fb36578471febabc36b433
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-24 15:50:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,19 +1,15 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String result = "";
         for(String[] info : db) {
             if(info[0].equals(id_pw[0])) {
                 if (!info[1].equals(id_pw[1])) {
-                    result = "wrong pw";
+                    return "wrong pw";
                 } else {
-                    result = "login";
+                    return  "login";
                 }
             }
         }
-        if(result.equals("")) {
-            result = "fail";
-        }
 
-        return result;
+        return "fail";
     }
 }
