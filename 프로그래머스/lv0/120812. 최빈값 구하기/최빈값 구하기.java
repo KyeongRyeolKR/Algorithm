0411b1f6940aa3d82e355b6e9c36f35e432f7371
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-27 11:05:06 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.74 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<array.length; i++) {
+            int count = 1;
+            for(int j=0; j<array.length; j++) {
+                if(i == j) continue;
+                if(array[i] == array[j]) count++;
+            }
+            map.put(array[i], count);
+        }
+
+        System.out.println(map);
+
+        int max = 0;
+        int name = 0;
+        for (int x : map.keySet()) {
+            if(map.get(x) > max) {
+                max = map.get(x);
+                name = x;
+            }
+        }
+
+        int count = 0;
+        for(int x : map.keySet()) {
+            if(map.get(x) == max) count++;
+        }
+
+        if(count > 1) return -1;
+
+        return name;
+    }
+}
