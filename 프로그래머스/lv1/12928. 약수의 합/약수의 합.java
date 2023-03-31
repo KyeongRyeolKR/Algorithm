610aca7283f154212fbc6ce5076be39751955b81
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-01 00:33:09 +0900

    [level 1] Title: 약수의 합, Time: 1.36 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,17 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        IntStream.rangeClosed(1,n)
+        return IntStream.rangeClosed(1,n)
                 .filter(e -> n % e == 0)
-                .forEach(list::add);
-        
-        return list.stream()
-                .mapToInt(Integer::intValue)
                 .sum();
     }
 }
