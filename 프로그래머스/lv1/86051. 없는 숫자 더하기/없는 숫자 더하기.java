3001d965ec98814dbeab9df3e9ab19c87eed05e4
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-02 19:38:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.13 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,20 +1,7 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] numbers) {
-        List<Integer> list = Arrays.stream(numbers)
-                .boxed()
-                .collect(Collectors.toList());
-
-        int sum = 0;
-        for(int i=0; i<10; i++) {
-            if(!list.contains(i)) {
-                sum += i;
-            }
-        }
-        
-        return sum;
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
