68012711b01fdf499bca1b4588fa8a8d0b4ef3a2
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-03 14:06:26 +0900

    [level 0] Title: 배열 자르기, Time: 2.36 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,13 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-
-        int subIndex = 0;
-        for (int i = num1; i <= num2; i++) {
-            answer[subIndex] = numbers[i];
-            subIndex++;
-        }
-
-        return answer;
+        return IntStream.rangeClosed(num1, num2)
+                .map(e -> numbers[e])
+                .toArray();
     }
 }
