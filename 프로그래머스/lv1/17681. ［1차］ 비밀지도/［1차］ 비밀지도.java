15a1c617df332aa65fead57bb6f4deaf84f82c6a
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-08 23:16:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.37 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,40 +1,15 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] nArr1 = new String[n];
-        String[] nArr2 = new String[n];
-
-        for(int i=0; i<arr1.length; i++) {
-            String temp = Integer.toBinaryString(arr1[i]);
-            while(temp.length() < n) {
-                temp = "0" + temp;
-            }
-            nArr1[i] = temp;
-        }
-
-        for(int i=0; i<arr1.length; i++) {
-            String temp = Integer.toBinaryString(arr2[i]);
-            while(temp.length() < n) {
-                temp = "0" + temp;
-            }
-            nArr2[i] = temp;
-        }
-
         String[] answer = new String[n];
-        for(int i=0; i<nArr1.length; i++) {
-            String temp = "";
-            for(int j=0; j<n; j++) {
-                if(nArr1[i].charAt(j) == '1' || nArr2[i].charAt(j) == '1') {
-                    temp += "#";
-                } else {
-                    temp += " ";
-                }
-            }
-            answer[i] = temp;
+        for(int i=0; i<n; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
         }
 
-//        System.out.println(Arrays.toString(nArr1));
-//        System.out.println(Arrays.toString(nArr2));
-//        System.out.println(Arrays.toString(answer));
+        for(int i=0; i<n; i++) {
+            answer[i] = String.format("%" + n + "s", answer[i]);
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
+        }
 
         return answer;
     }
