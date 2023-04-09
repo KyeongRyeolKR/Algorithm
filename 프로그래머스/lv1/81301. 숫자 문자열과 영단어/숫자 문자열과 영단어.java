16adb74385ef13df8804f989b395e833d9fdf07f
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-09 12:56:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.88 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,16 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String s) {
-        if(s.contains("zero")) s = s.replaceAll("zero", "0");
-        if(s.contains("one")) s = s.replaceAll("one", "1");
-        if(s.contains("two")) s = s.replaceAll("two", "2");
-        if(s.contains("three")) s = s.replaceAll("three", "3");
-        if(s.contains("four")) s = s.replaceAll("four", "4");
-        if(s.contains("five")) s = s.replaceAll("five", "5");
-        if(s.contains("six")) s = s.replaceAll("six", "6");
-        if(s.contains("seven")) s = s.replaceAll("seven", "7");
-        if(s.contains("eight")) s = s.replaceAll("eight", "8");
-        if(s.contains("nine")) s = s.replaceAll("nine", "9");
+        String[] name = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        return Integer.parseInt(s);
+        return Integer.parseInt(Arrays.stream(name)
+                .reduce(s, (str, n) -> str.replaceAll(n, Integer.toString(Arrays.asList(name).indexOf(n)))));
     }
 }
