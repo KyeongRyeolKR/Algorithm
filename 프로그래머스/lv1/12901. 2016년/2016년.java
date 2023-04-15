79a13b209a65ba09c6d772597e47a44ed19263a5
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-15 23:39:41 +0900

    [level 1] Title: 2016년, Time: 1.08 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,15 +1,7 @@
+import java.time.LocalDate;
+
 class Solution {
     public String solution(int a, int b) {
-        int[] dayOfMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] dayOfWeek = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-
-        int sum = -1;
-        for(int month=1; month<a; month++) {
-            sum += dayOfMonth[month];
-        }
-
-        sum += b;
-
-        return dayOfWeek[sum % 7];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
     }
 }
