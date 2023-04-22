368f2164db61cea59f94970724cccc2047e4b3bf
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-22 18:04:45 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String name) {
+        int upDownCount = 0;
+        int leftRightCount = name.length()-1;
+
+        int endA = 0;
+        for(int i=0; i<name.length(); i++) {
+            int case1 = Math.abs(name.charAt(i) - 'A');
+            int case2 = Math.abs(name.charAt(i) - 91);
+            upDownCount += Math.min(case1, case2);
+
+            endA = i+1;
+            while(endA < name.length() && name.charAt(endA) == 'A') {
+                endA++;
+            }
+
+            leftRightCount = Math.min(leftRightCount, i * 2 + name.length() - endA);
+            leftRightCount = Math.min(leftRightCount, (name.length() - endA) * 2 + i);
+        }
+
+        return upDownCount + leftRightCount;
+    }
+}
