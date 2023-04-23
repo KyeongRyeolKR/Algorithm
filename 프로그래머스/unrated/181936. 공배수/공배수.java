f1a1cb197ccc91142e57f486390fb7e6ffb62b4f
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-23 21:23:25 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,17 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int lcm = n * m / getGCD(n, m);
-
-        for(int i=1; i<=100; i++) {
-            int temp = lcm * i;
-            if(temp == number) return 1;
-        }
-
-        return 0;
-    }
-
-    public int getGCD(int num1, int num2) {
-        if(num2 == 0) return num1;
-        return getGCD(num2, num1%num2);
+        return (number % n == 0) && (number % m == 0) ? 1 : 0;
     }
 }
