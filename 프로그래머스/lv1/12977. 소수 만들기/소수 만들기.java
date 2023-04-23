acf7ff27bea713585c9f9050334338474d0e04e7
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-23 22:54:14 +0900

    [level 1] Title: 소수 만들기, Time: 1.32 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,29 +1,25 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int len = nums.length;
-        
-        // 3개의 수를 더해서 만들 수 있는 모든 경우를 구하기 위해 3중 for문 사용
-        for(int i=0; i<len-2; i++) {
-            for(int j=i+1; j<len-1; j++) {
-                for(int k=j+1; k<len; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if(isPrime(sum)) answer++; // 소수인 경우 answer 증가
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<nums.length-2; i++) {
+            for(int j=i+1; j<nums.length-1; j++) {
+                for(int k=j+1; k<nums.length; k++) {
+                    list.add(nums[i] + nums[j] + nums[k]);
                 }
             }
         }
-        
-        return answer;
+
+        return (int) list.stream()
+                .filter(this::isPrime)
+                .count();
     }
-    
-    // 소수 판별 함수
+
     public boolean isPrime(int num) {
-        if(num < 2) return false;
-        for(int i=2; i*i<=num; i++) {
-            if(num % i == 0) return false;
-        }
-        return true;
+        return IntStream.rangeClosed(1, num)
+                .filter(e -> num % e == 0)
+                .count() == 2;
     }
 }
