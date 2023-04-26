87a7ad08d47508d929d1e416985215e8ce575975
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-04-27 00:07:03 +0900

    [level 1] Title: 실패율, Time: 4.00 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -14,6 +14,8 @@ class Solution {
                 if (stage == stages[i]) fail++;
                 if (stage <= stages[i]) comeToStage++;
             }
+
+            // 아무도 해당 스테이지에 오지 못했을 경우는 실패율을 0으로 한다.
             if(comeToStage == 0) {
                 map.put(stage, 0.0);
             } else {
@@ -21,17 +23,23 @@ class Solution {
             }
         }
 
-        System.out.println(map);
+        System.out.println("저장된 map : " + map);
 
+        // Map -> List 변환
         List<Map.Entry<Integer, Double>> list = new ArrayList<>(map.entrySet());
-        list.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+        System.out.println("정렬 전 List : " + list);
 
+        list.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+        System.out.println("정렬 후 List : " + list);
+
+        // 정렬된 리스트에서 key를 가져와 순서대로 배열에 저장
         int[] answer = new int[N];
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i).getKey();
         }
 
-        System.out.println(Arrays.toString(answer));
+        System.out.println("실패율 기준 내림차순 된 스테이지 : " + Arrays.toString(answer));
+        System.out.println();
 
         return answer;
     }
