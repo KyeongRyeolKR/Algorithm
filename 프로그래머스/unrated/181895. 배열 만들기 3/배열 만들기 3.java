6f8cdf6eb41bcff3f6d3c34730f28eb717778640
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-05-19 17:26:45 +0900

    [unrated] Title: 배열 만들기 3, Time: 2.95 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            for (int i = interval[0]; i <= interval[1]; i++) {
+                list.add(arr[i]);
+            }
+        }
+
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
