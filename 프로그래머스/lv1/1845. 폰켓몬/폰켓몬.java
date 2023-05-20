37d1adfad3909b1546512fbae68fdf58b470eeae
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-05-20 22:55:33 +0900

    [level 1] Title: 폰켓몬, Time: 5.38 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] nums) {
+        int choice = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+        return Math.min(set.size(), choice);
+    }
+}
