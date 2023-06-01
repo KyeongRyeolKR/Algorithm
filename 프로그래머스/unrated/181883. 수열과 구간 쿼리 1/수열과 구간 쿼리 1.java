14ba4c2827f35e9c23e8da16f8528d3c7781b4e9
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-06-01 22:56:33 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 7.14 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        Arrays.stream(queries).flatMapToInt(query -> IntStream.rangeClosed(query[0], query[1])).forEach(j -> arr[j]++);
+        
+        return arr;
+    }
+}
