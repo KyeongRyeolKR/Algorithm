dafef227b5db62c934d7351b3c1d9f32056a2040
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-07-06 23:08:00 +0900

    [Silver I] Title: 안전 영역, Time: 492 ms, Memory: 31956 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,73 +1,59 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
     static int N;
     static int[][] map;
     static boolean[][] visited;
-
-    // 하, 우, 상, 좌
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0 , -1};
+    static int[] dr = {-1, 0, 1, 0}; // 시계 방향
+    static int[] dc = {0, 1, 0, -1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
 
-        map = new int[N][N];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
+        map = new int[N+1][N+1];
+        int maxHeight = Integer.MIN_VALUE;
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
                 map[i][j] = sc.nextInt();
+                if(map[i][j] > maxHeight) {
+                    maxHeight = map[i][j];
+                }
             }
         }
 
-        int max = 0;
-        // 아무곳도 잠기지 않을 수 있기에 잠기는 물높이를 0부터 시작해서 최대 물높이 100까지 반복
-        for(int i=0; i<=100; i++) {
-            // 반복 때 마다 visited 배열을 초기화 해줘야함
-            visited = new boolean[N][N];
-            // 안전영역이 더 많은 것을 max에 저장
-            max = Math.max(max, bfs(i));
-        }
-
-        System.out.println(max);
-    }
-
-    private static int bfs(int height) {
-        int area = 0;
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                // 물 높이보다 작거나 같으면 침수니까 필요 없고, 이미 체크했던 곳이면 필요 없음
-                if(map[i][j] <= height || visited[i][j]) continue;
-
-                Queue<int[]> queue = new LinkedList<>();
-                visited[i][j] = true;
-                queue.add(new int[]{i, j});
-
-                while(!queue.isEmpty()) {
-                    int[] point = queue.poll();
-
-                    for(int k=0; k<4; k++) {
-                        int cx = point[0] + dx[k];
-                        int cy = point[1] + dy[k];
-
-                        // 범위를 벗어나면 다음
-                        if(cx < 0 || cx >= N || cy <0 || cy >= N) continue;
-                        // 바뀐 지점이 침수당할 높이거나, 이미 체크했던 곳이면 다음
-                        if(map[cx][cy] <= height || visited[cx][cy]) continue;
-
-                        visited[cx][cy] = true;
-                        queue.add(new int[]{cx, cy});
+        int maxArea = Integer.MIN_VALUE;
+        for(int rain=0; rain<=maxHeight; rain++) {
+            visited = new boolean[N+1][N+1];
+            int area = 0;
+            for(int i=1; i<=N; i++) {
+                for(int j=1; j<=N; j++) {
+                    if(!visited[i][j] && map[i][j] > rain) {
+                        area += dfs(i, j, rain);
                     }
                 }
+            }
+            maxArea = Math.max(maxArea, area);
+        }
 
-                area++;
+        System.out.println(maxArea);
+    }
+
+    private static int dfs(int row, int col, int rain) {
+        visited[row][col] = true;
+
+        for(int i=0; i<4; i++) {
+            int cr = row + dr[i];
+            int cc = col + dc[i];
+            if(cr >= 1 && cr <= N && cc >= 1 && cc <= N) {
+                if(!visited[cr][cc] && map[cr][cc] > rain) {
+                    dfs(cr, cc, rain);
+                }
             }
         }
 
-        return area;
+        return 1;
     }
 }
