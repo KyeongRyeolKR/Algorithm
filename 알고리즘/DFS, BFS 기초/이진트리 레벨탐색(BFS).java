d04c88594b53d1f085e23fc934eb03b931a0fa24
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-07-26 21:54:21 +0900

    이진트리 레벨탐색(BFS)

--- 알고리즘/DFS, BFS 기초/이진트리 레벨탐색(BFS).java
+++ 알고리즘/DFS, BFS 기초/이진트리 레벨탐색(BFS).java
@@ -1,8 +1,13 @@
 import java.util.*;
 
+/**
+ * 이진트리 BFS의 동작 원리 구현
+ * -> 같은 레벨의 노드들을 전부 탐색한 후, 다음 레벨의 노드들을 탐색한다.
+ */
 public class Main {
     Node root;
 
+    // 기본 값(트리) 세팅
     public static void main(String[] args) {
         Main tree = new Main();
 
@@ -19,13 +24,15 @@ public class Main {
 
     public void bfs(Node root) {
         Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
+        queue.offer(root);  // 탐색 시작 노드
 
-        int level = 0;
+        int level = 0;  // 현재 레벨
         while (!queue.isEmpty()) {
-            int len = queue.size();
+            int len = queue.size(); // 현재 레벨에서 탐색할 노드의 개수
             System.out.print(level + " : ");
 
+            // 같은 레벨의 노드들을 전부 순회하며 탐색
+            // 탐색중인 노드가 자식 노드를 가지고 있다면, 큐에 추가
             for(int i=0; i<len; i++) {
                 Node cur = queue.poll();
                 System.out.print(cur.data + " ");
@@ -38,7 +45,7 @@ public class Main {
                 }
             }
 
-            level++;
+            level++;    // 현재 레벨에서의 모든 탐색이 끝났으니, 다음 레벨로 올림
             System.out.println();
         }
     }
