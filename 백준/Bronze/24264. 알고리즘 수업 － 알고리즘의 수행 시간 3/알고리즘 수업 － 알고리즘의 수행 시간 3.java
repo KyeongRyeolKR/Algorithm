f6ad13f5e069d0e14dc6fba4983a7637fac35c3b
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-01-05 23:58:54 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 220 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Long n= sc.nextLong();
+        System.out.println(n*n);
+        System.out.println('2');
+    }
+}
