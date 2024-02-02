592c5211daed8660fc1505c4b158a0463f9266e0
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-02-02 17:46:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 2.76 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,22 @@
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
-        int next = 0;
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < number.length() - k; i++) {
-            int max = 0;
-
-            for (int j = index; j <= i + k; j++) {
-                int current = number.charAt(j) - '0';
-
-                if (max < current) {
-                    max = current;
-                    next = j;
-                }
+        for(int i=0; i<number.length(); i++) {
+            char now = number.charAt(i);
+            while(!stack.isEmpty() && stack.peek() < now && k > 0) {
+                stack.pop();
+                k--;
             }
-            sb.append(max);
-            index = next + 1;
+            stack.push(now);
         }
-        return sb.toString();
+
+        return stack.stream()
+                .limit(number.length()-k)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
