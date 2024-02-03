e81c4b008367a92d54b952d331b8bba6b853e9b5
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-02-03 15:56:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.88 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,20 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<participant.length; i++) {
-            if(map.containsKey(participant[i])) {
-                map.put(participant[i], map.get(participant[i]) + 1);
-            } else {
-                map.put(participant[i], 1);
-            }
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
 
-        for(int i=0; i<completion.length; i++) {
-            if(map.containsKey(completion[i])) {
-                if (map.get(completion[i]) == 1) {
-                    map.remove(completion[i]);
-                } else {
-                    map.put(completion[i], map.get(completion[i]) - 1);
-                }
-            }
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
         }
 
-        return map.keySet().iterator().next();
+        return map.keySet().stream()
+                .filter(e -> map.get(e) == 1)
+                .collect(Collectors.joining());
     }
 }
