65dd9f8898d9144fc262e4296afbf1aa3062f94c
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-02-22 20:20:08 +0900

    [level 2] Title: 구명보트, Time: 9.04 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int lt = 0;
+        int rt = people.length-1;
+
+        while(lt <= rt) {
+            if(people[lt] + people[rt] <= limit) {
+                lt++;
+            }
+            
+            rt--;
+            
+            answer++;
+        }
+
+        return answer;
+    }
+}
