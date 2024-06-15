5211f5ae948da9d14c0a0459bf46dde83ae47a98
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-06-15 22:41:46 +0900

    [Silver I] Title: 트럭, Time: 304 ms, Memory: 20732 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+        int L = sc.nextInt();
+        
+        Queue<Integer> truck = new LinkedList<>();
+        for(int i=0; i<n; i++) {
+            truck.offer(sc.nextInt());
+        }
+
+        int time = 0;
+        int bw = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<w; i++) {
+            q.add(0);
+        }
+
+        while(!q.isEmpty()) {
+            time++;
+            bw -= q.poll();
+            if(!truck.isEmpty()) {
+                if(truck.peek() + bw <= L) {
+                    bw+=truck.peek();
+                    q.offer(truck.poll());
+                } else {
+                    q.offer(0);
+                }
+            }
+        }
+        
+        System.out.println(time);
+    }
+}
