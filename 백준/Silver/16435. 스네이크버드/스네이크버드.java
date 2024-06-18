d55f78b89fae9f0edaba1652233277d04f60a06b
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-06-18 23:30:24 +0900

    [Silver V] Title: 스네이크버드, Time: 272 ms, Memory: 20456 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int snake = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+        }
+
+		Arrays.sort(arr);
+        
+		for (int i = 0; i < arr.length; i++) {
+			if (snake >= arr[i]) {
+				++snake;
+            }
+		}
+        
+		System.out.println(snake);
+	}
+}
