aa1698c1af864660cd817fb23cf767100568c658
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-07-09 23:34:24 +0900

    [level 2] Title: n^2 배열 자르기, Time: 19.08 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<Long> solution(int n, long left, long right) {
+        List<Long> list = new ArrayList<>();
+        
+        long cnt = left / n * n;
+        for(long i = left / n; i < n; i++) {
+            if(cnt > right) break;
+            for(long j = 0; j < n; j++) {
+                if(cnt > right) {
+                    break;
+                }
+                
+                if(cnt >= left) {
+                    list.add(Math.max(i, j) + 1);
+                }
+                
+                cnt++;
+            }
+        }
+        
+        return list;
+    }
+}
