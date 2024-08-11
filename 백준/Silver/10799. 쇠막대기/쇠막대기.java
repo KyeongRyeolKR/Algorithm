def7a3ef8707a86d044cdbb736900f367fbcc97c
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-08-11 22:00:42 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16460 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        int result = 0;
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');
+                continue;
+            }
+
+            if (input.charAt(i) == ')') {
+                stack.pop();
+
+                if (input.charAt(i-1) == '(') {
+                    result += stack.size();
+                } else {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
