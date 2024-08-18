089b74cb209a50ed0109feb839a39ff6e96ffab6
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-08-18 23:58:24 +0900

    [Silver V] Title: 올림픽, Time: 116 ms, Memory: 14692 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int num = Integer.parseInt(st.nextToken());
+        int cer = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[num+1][4];
+        int rank = 1;
+        for(int i=1; i<=num; i++) {
+            st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            arr[index][0] = g;
+            arr[index][1] = s;
+            arr[index][2] = b;
+        }
+
+        for(int i=1; i<=num; i++) {
+            if(arr[i][0] > arr[cer][0]) {
+                rank++;
+            } else if(arr[i][0] == arr[cer][0] && arr[i][1] > arr[cer][1]) {
+                rank++;
+            } else if(arr[i][0] == arr[cer][0] && arr[i][1] == arr[cer][1] && arr[i][2] > arr[cer][2]) {
+                rank++;
+            }
+        }
+
+        System.out.println(rank);
+    }
+}
