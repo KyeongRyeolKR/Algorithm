2da92846050e138ecba3982cc3a54a6e634adb48
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2024-08-26 22:18:43 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 188 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,42 +1,28 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
+        int t = sc.nextInt();
+        for(int T=0; T<t; T++) {
+            int n = sc.nextInt();
 
-        while (T -- > 0) {
-
-            HashMap<String, Integer> hm = new HashMap<>();
-
-            int N = sc.nextInt();
-
-            while (N-- > 0) {
-
-                sc.next();
-
-                String kind = sc.next();
-
-                if (hm.containsKey(kind)) {
-                    hm.put(kind, hm.get(kind) + 1);
-                }
-                else {
-                    hm.put(kind, 1);
-                }
+            Map<String, Integer> map = new HashMap<>();
+            for(int i=0; i<n; i++) {
+                String key = sc.next();
+                String value = sc.next();
+                map.put(value, map.getOrDefault(value, 0) + 1);
             }
 
-            int result = 1;
-            for (int val : hm.values()) {
-                result *= (val + 1);
+            int answer = 1;
+            for(int value : map.values()) {
+                answer *= value + 1;
             }
 
-            System.out.println(result - 1);
-
+            System.out.println(answer-1);
         }
-
     }
-
 }
